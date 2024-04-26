afb1c8a7c65656de040b4a9352330b030730a80d
jigz-paisen <103289547+jigz-paisen@users.noreply.github.com>
2024-04-26 13:57:01 +0100

    Add files via upload

---
+++ WinFormsApp11/WinFormsApp11/Form1.Designer.vb
@@ -0,0 +1,332 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        InvDataGroupBox = New GroupBox()
+        itemComboBox = New ComboBox()
+        wholesaleCostTextBox = New TextBox()
+        quantityTextBox = New TextBox()
+        ItemQuantLabel = New Label()
+        WholesaleLabel = New Label()
+        ItemLabel = New Label()
+        itemListBox = New ListBox()
+        Label1 = New Label()
+        totalInventoryValueTextBox = New TextBox()
+        TotalValueLabel = New Label()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        JToolStripMenuItem = New ToolStripMenuItem()
+        InventoriesToolStripMenuItem = New ToolStripMenuItem()
+        AddToItemListingToolStripMenuItem = New ToolStripMenuItem()
+        ClearListingToolStripMenuItem = New ToolStripMenuItem()
+        CountItemListingToolStripMenuItem = New ToolStripMenuItem()
+        AddItemToComboBoxToolStripMenuItem = New ToolStripMenuItem()
+        DeleteComboBoxItemToolStripMenuItem = New ToolStripMenuItem()
+        ColorsAndFontsToolStripMenuItem = New ToolStripMenuItem()
+        SetBackgroundColorToolStripMenuItem = New ToolStripMenuItem()
+        changeFontButton = New ToolStripMenuItem()
+        HelpToolStripMenuItem = New ToolStripMenuItem()
+        AboutToolStripMenuItem = New ToolStripMenuItem()
+        formContextMenu = New ContextMenuStrip(components)
+        ColorToolStripMenuItem = New ToolStripMenuItem()
+        FontToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        InvDataGroupBox.SuspendLayout()
+        MenuStrip1.SuspendLayout()
+        formContextMenu.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' InvDataGroupBox
+        ' 
+        InvDataGroupBox.Controls.Add(itemComboBox)
+        InvDataGroupBox.Controls.Add(wholesaleCostTextBox)
+        InvDataGroupBox.Controls.Add(quantityTextBox)
+        InvDataGroupBox.Controls.Add(ItemQuantLabel)
+        InvDataGroupBox.Controls.Add(WholesaleLabel)
+        InvDataGroupBox.Controls.Add(ItemLabel)
+        InvDataGroupBox.Location = New Point(44, 27)
+        InvDataGroupBox.Name = "InvDataGroupBox"
+        InvDataGroupBox.Size = New Size(431, 116)
+        InvDataGroupBox.TabIndex = 0
+        InvDataGroupBox.TabStop = False
+        InvDataGroupBox.Text = "Inventory Data"
+        ' 
+        ' itemComboBox
+        ' 
+        itemComboBox.AllowDrop = True
+        itemComboBox.FormattingEnabled = True
+        itemComboBox.Items.AddRange(New Object() {"TaylorMade R7 Driver RH 10-degree", "TaylorMade R7 Driver LH 10-degree", "TaylorMade R7 Driver RH 9-degree", "Titleist 907D2 Driver RH 9.5-degree", "Titleist 907D2 Driver RH 10.5-degree", "King Cobra HS9 X Driver RH 9-degree", "King Cobra HS9 X Driver LH 9-degree", "Adams Redline RPM Driver RH 10-degree", ""})
+        itemComboBox.Location = New Point(123, 26)
+        itemComboBox.Name = "itemComboBox"
+        itemComboBox.Size = New Size(213, 23)
+        itemComboBox.TabIndex = 6
+        ' 
+        ' wholesaleCostTextBox
+        ' 
+        wholesaleCostTextBox.Location = New Point(123, 52)
+        wholesaleCostTextBox.Name = "wholesaleCostTextBox"
+        wholesaleCostTextBox.Size = New Size(127, 23)
+        wholesaleCostTextBox.TabIndex = 4
+        wholesaleCostTextBox.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' quantityTextBox
+        ' 
+        quantityTextBox.Location = New Point(123, 80)
+        quantityTextBox.Name = "quantityTextBox"
+        quantityTextBox.Size = New Size(127, 23)
+        quantityTextBox.TabIndex = 5
+        quantityTextBox.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' ItemQuantLabel
+        ' 
+        ItemQuantLabel.AutoSize = True
+        ItemQuantLabel.Location = New Point(11, 83)
+        ItemQuantLabel.Name = "ItemQuantLabel"
+        ItemQuantLabel.Size = New Size(97, 15)
+        ItemQuantLabel.TabIndex = 2
+        ItemQuantLabel.Text = "Quantity of Item:"
+        ' 
+        ' WholesaleLabel
+        ' 
+        WholesaleLabel.AutoSize = True
+        WholesaleLabel.Location = New Point(11, 55)
+        WholesaleLabel.Name = "WholesaleLabel"
+        WholesaleLabel.Size = New Size(91, 15)
+        WholesaleLabel.TabIndex = 1
+        WholesaleLabel.Text = "Wholesale Cost:"
+        ' 
+        ' ItemLabel
+        ' 
+        ItemLabel.AutoSize = True
+        ItemLabel.Location = New Point(11, 29)
+        ItemLabel.Name = "ItemLabel"
+        ItemLabel.Size = New Size(68, 15)
+        ItemLabel.TabIndex = 0
+        ItemLabel.Text = "Select Item:"
+        ' 
+        ' itemListBox
+        ' 
+        itemListBox.FormattingEnabled = True
+        itemListBox.ItemHeight = 15
+        itemListBox.Location = New Point(44, 177)
+        itemListBox.Name = "itemListBox"
+        itemListBox.Size = New Size(431, 79)
+        itemListBox.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(44, 159)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(125, 15)
+        Label1.TabIndex = 4
+        Label1.Text = "Inventory Item Listing:"
+        ' 
+        ' totalInventoryValueTextBox
+        ' 
+        totalInventoryValueTextBox.Location = New Point(375, 270)
+        totalInventoryValueTextBox.Name = "totalInventoryValueTextBox"
+        totalInventoryValueTextBox.ReadOnly = True
+        totalInventoryValueTextBox.Size = New Size(100, 23)
+        totalInventoryValueTextBox.TabIndex = 5
+        totalInventoryValueTextBox.TabStop = False
+        totalInventoryValueTextBox.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' TotalValueLabel
+        ' 
+        TotalValueLabel.AutoSize = True
+        TotalValueLabel.Location = New Point(230, 273)
+        TotalValueLabel.Name = "TotalValueLabel"
+        TotalValueLabel.Size = New Size(119, 15)
+        TotalValueLabel.TabIndex = 6
+        TotalValueLabel.Text = "Total Inventory Value:"
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, InventoriesToolStripMenuItem, ColorsAndFontsToolStripMenuItem, HelpToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(640, 24)
+        MenuStrip1.TabIndex = 7
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {JToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "&File"
+        ' 
+        ' JToolStripMenuItem
+        ' 
+        JToolStripMenuItem.Name = "JToolStripMenuItem"
+        JToolStripMenuItem.Size = New Size(93, 22)
+        JToolStripMenuItem.Text = "E&xit"
+        ' 
+        ' InventoriesToolStripMenuItem
+        ' 
+        InventoriesToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {AddToItemListingToolStripMenuItem, ClearListingToolStripMenuItem, CountItemListingToolStripMenuItem, AddItemToComboBoxToolStripMenuItem, DeleteComboBoxItemToolStripMenuItem})
+        InventoriesToolStripMenuItem.Name = "InventoriesToolStripMenuItem"
+        InventoriesToolStripMenuItem.Size = New Size(69, 20)
+        InventoriesToolStripMenuItem.Text = "&Inventory"
+        ' 
+        ' AddToItemListingToolStripMenuItem
+        ' 
+        AddToItemListingToolStripMenuItem.Name = "AddToItemListingToolStripMenuItem"
+        AddToItemListingToolStripMenuItem.Size = New Size(203, 22)
+        AddToItemListingToolStripMenuItem.Text = "&Add Item To Listing"
+        ' 
+        ' ClearListingToolStripMenuItem
+        ' 
+        ClearListingToolStripMenuItem.Name = "ClearListingToolStripMenuItem"
+        ClearListingToolStripMenuItem.Size = New Size(203, 22)
+        ClearListingToolStripMenuItem.Text = "&Clear Listing "
+        ' 
+        ' CountItemListingToolStripMenuItem
+        ' 
+        CountItemListingToolStripMenuItem.Name = "CountItemListingToolStripMenuItem"
+        CountItemListingToolStripMenuItem.Size = New Size(203, 22)
+        CountItemListingToolStripMenuItem.Text = "C&ount Item Listing"
+        ' 
+        ' AddItemToComboBoxToolStripMenuItem
+        ' 
+        AddItemToComboBoxToolStripMenuItem.Name = "AddItemToComboBoxToolStripMenuItem"
+        AddItemToComboBoxToolStripMenuItem.Size = New Size(203, 22)
+        AddItemToComboBoxToolStripMenuItem.Text = "A&dd Item to Combo Box"
+        ' 
+        ' DeleteComboBoxItemToolStripMenuItem
+        ' 
+        DeleteComboBoxItemToolStripMenuItem.Name = "DeleteComboBoxItemToolStripMenuItem"
+        DeleteComboBoxItemToolStripMenuItem.Size = New Size(203, 22)
+        DeleteComboBoxItemToolStripMenuItem.Text = "&Delete Combo Box Item"
+        ' 
+        ' ColorsAndFontsToolStripMenuItem
+        ' 
+        ColorsAndFontsToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {SetBackgroundColorToolStripMenuItem, changeFontButton})
+        ColorsAndFontsToolStripMenuItem.Name = "ColorsAndFontsToolStripMenuItem"
+        ColorsAndFontsToolStripMenuItem.Size = New Size(108, 20)
+        ColorsAndFontsToolStripMenuItem.Text = "&Colors and Fonts"
+        ' 
+        ' SetBackgroundColorToolStripMenuItem
+        ' 
+        SetBackgroundColorToolStripMenuItem.Name = "SetBackgroundColorToolStripMenuItem"
+        SetBackgroundColorToolStripMenuItem.Size = New Size(189, 22)
+        SetBackgroundColorToolStripMenuItem.Text = "Set &Background Color"
+        ' 
+        ' changeFontButton
+        ' 
+        changeFontButton.Name = "changeFontButton"
+        changeFontButton.Size = New Size(189, 22)
+        changeFontButton.Text = "Set &Font"
+        ' 
+        ' HelpToolStripMenuItem
+        ' 
+        HelpToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {AboutToolStripMenuItem})
+        HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        HelpToolStripMenuItem.Size = New Size(44, 20)
+        HelpToolStripMenuItem.Text = "&Help"
+        ' 
+        ' AboutToolStripMenuItem
+        ' 
+        AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        AboutToolStripMenuItem.Size = New Size(180, 22)
+        AboutToolStripMenuItem.Text = "&About"
+        ' 
+        ' formContextMenu
+        ' 
+        formContextMenu.Items.AddRange(New ToolStripItem() {ColorToolStripMenuItem, FontToolStripMenuItem, ExitToolStripMenuItem})
+        formContextMenu.Name = "ContextMenuStrip1"
+        formContextMenu.Size = New Size(181, 92)
+        ' 
+        ' ColorToolStripMenuItem
+        ' 
+        ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        ColorToolStripMenuItem.Size = New Size(180, 22)
+        ColorToolStripMenuItem.Text = "Color"
+        ' 
+        ' FontToolStripMenuItem
+        ' 
+        FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        FontToolStripMenuItem.Size = New Size(180, 22)
+        FontToolStripMenuItem.Text = "Font"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(180, 22)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(640, 303)
+        Controls.Add(TotalValueLabel)
+        Controls.Add(totalInventoryValueTextBox)
+        Controls.Add(Label1)
+        Controls.Add(itemListBox)
+        Controls.Add(InvDataGroupBox)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        InvDataGroupBox.ResumeLayout(False)
+        InvDataGroupBox.PerformLayout()
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        formContextMenu.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents InvDataGroupBox As GroupBox
+    Friend WithEvents wholesaleCostTextBox As TextBox
+    Friend WithEvents quantityTextBox As TextBox
+    Friend WithEvents ItemQuantLabel As Label
+    Friend WithEvents WholesaleLabel As Label
+    Friend WithEvents ItemLabel As Label
+    Friend WithEvents itemListBox As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents itemComboBox As ComboBox
+    Friend WithEvents totalInventoryValueTextBox As TextBox
+    Friend WithEvents TotalValueLabel As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents JToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InventoriesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorsAndFontsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddToItemListingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearListingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CountItemListingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddItemToComboBoxToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteComboBoxItemToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SetBackgroundColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents changeFontButton As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents formContextMenu As ContextMenuStrip
+    Friend WithEvents ColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ WinFormsApp11/WinFormsApp11/Form1.vb
@@ -0,0 +1,221 @@
+﻿Public Class Form1
+    Private Sub JToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles JToolStripMenuItem.Click
+        Dim result As DialogResult = MessageBox.Show("Are you sure?", "Exit", MessageBoxButtons.YesNo)
+        If result = DialogResult.No Then
+            MessageBox.Show("Returning To App")
+        ElseIf result = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Function ValidData() As Boolean
+        ' Business rule #1: An inventory item type has been selected from the itemComboBox control.
+        If itemComboBox.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select an inventory item type.")
+            Return False
+        End If
+
+        ' Business rule #2: The wholesaleCostTextBox control contains a numeric value greater than zero.
+        Dim wholesaleCost As Decimal
+        If Not Decimal.TryParse(wholesaleCostTextBox.Text, wholesaleCost) OrElse wholesaleCost <= 0 Then
+            MessageBox.Show("Please enter a valid wholesale cost greater than zero.")
+            Return False
+        End If
+
+        ' Business rule #3: The quantityTextBox control contains a numeric value greater than zero.
+        Dim quantity As Integer
+        If Not Integer.TryParse(quantityTextBox.Text, quantity) OrElse quantity <= 0 Then
+            MessageBox.Show("Please enter a valid quantity greater than zero.")
+            Return False
+        End If
+
+        ' All business rules passed, data is valid
+        Return True
+
+    End Function
+
+    ' Define a module-level variable to store the total inventory value
+    Private totalInventoryValue As Decimal = 0
+
+    Private Sub AddToItemListingToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        ' Call the ValidData function to validate the input
+        If ValidData() Then
+            ' Process the data and add it to the item listing
+            Dim selectedItem As String = itemComboBox.SelectedItem.ToString()
+            Dim wholesaleCost As Decimal = Decimal.Parse(wholesaleCostTextBox.Text)
+            Dim quantity As Integer = Integer.Parse(quantityTextBox.Text)
+
+            ' Calculate the item value (quantity times price)
+            Dim itemValue As Decimal = wholesaleCost * quantity
+
+            ' Add the item and item value to the itemListBox
+            Dim listItem As String = selectedItem & " - " & itemValue.ToString("C")
+            itemListBox.Items.Add(listItem)
+
+            ' Accumulate the item value to the total inventory value
+            totalInventoryValue += itemValue
+
+            ' Update the display of the running total inventory value
+            totalInventoryValueTextBox.Text = totalInventoryValue.ToString("C")
+
+            ' Clear the text property for the input controls
+            itemComboBox.SelectedIndex = -1
+            itemComboBox.Text = ""
+            wholesaleCostTextBox.Clear()
+            quantityTextBox.Clear()
+
+            ' Set the focus to the itemComboBox
+            itemComboBox.Focus()
+        End If
+    End Sub
+
+    Private Sub ClearListingToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearListingToolStripMenuItem.Click
+        ' Display a confirmation message box
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to start a new inventory session? This will clear the item list.", "Clear Item Listing", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+
+        If result = DialogResult.Yes Then
+            ' Unselect the itemComboBox
+            itemComboBox.SelectedIndex = -1
+
+            ' Clear the text property of the itemComboBox and TextBox controls
+            itemComboBox.Text = ""
+            wholesaleCostTextBox.Clear()
+            quantityTextBox.Clear()
+
+            ' Clear the contents of the itemListBox
+            itemListBox.Items.Clear()
+
+            ' Reset the total inventory value
+            totalInventoryValue = 0
+
+            ' Update the display of the running total inventory value
+            totalInventoryValueTextBox.Text = totalInventoryValue.ToString("C")
+
+            ' Set the focus to the itemComboBox
+            itemComboBox.Focus()
+        End If
+    End Sub
+
+    Private Sub CountItemListingToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CountItemListingToolStripMenuItem.Click
+
+        Dim itemCount As Integer = itemComboBox.Items.Count
+
+        Dim message As String
+        If itemCount = 0 Then
+            message = "There are no inventory items available."
+        ElseIf itemCount = 1 Then
+            message = "There is 1 inventory item available."
+        Else
+            message = "There are " & itemCount & " inventory items available."
+        End If
+
+        MessageBox.Show(message, "Inventory Item Count", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub AddItemToComboBoxToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddItemToComboBoxToolStripMenuItem.Click
+        Dim newItem As String = itemComboBox.Text.Trim()
+
+        ' Check if the new item is blank
+        If newItem = "" Then
+            MessageBox.Show("Please enter a non-blank item name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Check for duplicates
+        If itemComboBox.Items.Cast(Of String)().Any(Function(item) String.Equals(item, newItem, StringComparison.OrdinalIgnoreCase)) Then
+            MessageBox.Show("This item already exists in the inventory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Add the new item to the ComboBox and sort the items
+        itemComboBox.Items.Add(newItem)
+        itemComboBox.Sorted = True
+
+        ' Clear the TextBox portion of the ComboBox
+        itemComboBox.Text = ""
+
+        ' Set the focus back to the ComboBox
+        itemComboBox.Focus()
+    End Sub
+
+    Private Sub DeleteComboBoxItemToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteComboBoxItemToolStripMenuItem.Click
+
+        ' Check if an item is selected in the ComboBox
+        If itemComboBox.SelectedIndex = -1 Then
+            MessageBox.Show("Please select an item to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Delete the selected item from the ComboBox
+        itemComboBox.Items.RemoveAt(itemComboBox.SelectedIndex)
+
+        ' Clear the Text property of the ComboBox
+        itemComboBox.Text = ""
+
+    End Sub
+
+    Private Sub SetBackgroundColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetBackgroundColorToolStripMenuItem.Click
+        ' Create a new instance of ColorDialog
+        Dim colorDialog As New ColorDialog()
+
+        ' Show the ColorDialog to the user
+        Dim result As DialogResult = colorDialog.ShowDialog()
+
+        ' If the user selects a color and clicks OK, change the form's background color
+        If result = DialogResult.OK Then
+            Me.BackColor = colorDialog.Color
+        End If
+    End Sub
+
+    Private Sub changeFontButton_Click(sender As Object, e As EventArgs) Handles changeFontButton.Click
+        Dim fontDialog As New FontDialog()
+
+        ' Show the FontDialog to the user
+        Dim result As DialogResult = fontDialog.ShowDialog()
+
+        ' If the user selects a font and clicks OK, change the font for all controls on the form
+        If result = DialogResult.OK Then
+            SetFontForControlAndChildren(Me, fontDialog.Font)
+        End If
+    End Sub
+
+    Private Sub SetFontForControlAndChildren(parentControl As Control, font As Font)
+        ' Set the font for the parent control
+        parentControl.Font = font
+
+        For Each control As Control In parentControl.Controls
+            SetFontForControlAndChildren(control, font)
+        Next
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        If e.Button = MouseButtons.Right Then
+            formContextMenu.Show(Me, e.Location)
+        End If
+    End Sub
+    Private Sub ShowNameAndDateTimeButton_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        ' Get the current date and time
+        Dim currentDateTime As String = DateTime.Now.ToString()
+
+        Dim message As String = "Your Name: [Your Name]" & vbCrLf & "Current Date/Time: " & currentDateTime
+
+        MessageBox.Show(message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+
+        Dim fontDialog As New FontDialog()
+
+        ' Show the FontDialog to the user
+        Dim result As DialogResult = fontDialog.ShowDialog()
+
+        ' If the user selects a font and clicks OK, change the font for all controls on the form
+        If result = DialogResult.OK Then
+            SetFontForControlAndChildren(Me, fontDialog.Font)
+        End If
+
+    End Sub
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
